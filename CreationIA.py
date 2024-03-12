894cff698e37cb04974a302dc414ca3165307d08
Skyman28 <107101306+JulianCoynel@users.noreply.github.com>
2024-03-12 14:10:05 -0400

    ajout fichier création et test

---
+++ CreationIA.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+
