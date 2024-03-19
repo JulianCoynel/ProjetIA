2b9a0993db39068211b5c0a8daaa2cbc45d1ccbf
Skyman28 <107101306+JulianCoynel@users.noreply.github.com>
2024-03-18 21:56:34 -0400

    ajout test avec tuto de tensorflow

--- CreationIA.py
+++ CreationIA.py
@@ -11,4 +11,8 @@ from tensorflow.keras import layers
 from tensorflow.keras import models
 from IPython import display
 
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
 
